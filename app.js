e8b3671344fc5729872dfbe60e5cda586cf471ff
Moh. Nadira Fabyansyah <168730539+DyraDev@users.noreply.github.com>
2024-05-19 13:49:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware untuk parsing JSON
 app.use(express.json());
+
+// Middleware CORS
+app.use(cors());
 
 // Rute dasar
 app.get('/', (req, res) => {
